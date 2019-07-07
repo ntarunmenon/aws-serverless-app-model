4ba7049c19cc4446465fac6c4a1f6fea87d7fe57
Arun Menon <arun.menon@qut.edu.au>
2019-07-07 12:03:53 +1000

    Add API Gateway

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@
 console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
-    callback(null, "event.key1");  // Echo back the first key value
-    //callback('Something went wrong');
+    const done = (err, res) => callback(null, {
+        statusCode: err ? '400' : '200',
+        body: err ? err.message : JSON.stringify(res),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    done(null, {hello: "world"})
 };
