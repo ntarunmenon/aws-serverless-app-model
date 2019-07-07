48673500292d979eb70c5c2b6640216e07d4bf37
Arun Menon <arun.menon@qut.edu.au>
2019-07-07 13:21:36 +1000

    Throw in DynamoDB

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 
 console.log('Loading function');
 
+var AWS = require("aws-sdk");
+AWS.config.update({region: process.env['REGION_NAME']});
+
+const tableName = process.env['TABLE_NAME']
+const dynamo = new AWS.DynamoDB();
+
 exports.handler = (event, context, callback) => {
     const done = (err, res) => callback(null, {
         statusCode: err ? '400' : '200',
@@ -10,6 +16,16 @@ exports.handler = (event, context, callback) => {
             'Content-Type': 'application/json',
         },
     });
-
-    done(null, {hello: "world"})
+const tableName = process.env['TABLE_NAME']
+var params = {
+  TableName: tableName
+}
+var resData;
+dynamo.scan(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     {
+      console.log(data.Items);
+      done(null, data.Items)
+  }           // successful response
+});
 };
